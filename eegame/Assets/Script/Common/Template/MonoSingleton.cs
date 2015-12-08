f6c0be681d73a984da1505ede0a1e18e9fd07b68
JackLN <linyn0612@icloud.com>
2015-12-08 16:22:31 +0800

    add template

---
+++ eegame/Assets/Script/Common/Template/MonoSingleton.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+namespace eegame
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+    {
+	private static  T m_Instance = null;
+	public static T Instance
+	{
+	    get
+	    {
+		if(m_Instance == null)
+		{
+		    m_Instance = GameObject.FindObjectOfType(typeof(T)) as T;
+		    if(m_Instance == null)
+		    {
+			m_Instance = new GameObject("Singleton<"+typeof(T).ToString()+">",typeof(T)).GetComponent<T>();
+			m_Instance.Init();
+		    }
+		}
+		return m_Instance;
+	    }
+	}
+
+	private void Awake()
+	{
+	    if(m_Instance == null)
+	    {
+		m_Instance = this as T;
+	    }
+	}
+	public virtual void Init()
+	{
+	    DontDestroyOnLoad(gameObject);
+	}
+	private void OnApplicationQuit()
+	{
+	    m_Instance = null;
+	}
+    }
+}
