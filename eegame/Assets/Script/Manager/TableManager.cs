869223edae7b6ae5eff10e5b78636976009255d7
JackLN <linyn0612@icloud.com>
2015-12-26 21:23:41 +0800

    + TableManager

--- eegame/Assets/Script/Manager/TableManager.cs
+++ eegame/Assets/Script/Manager/TableManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 using eegame;
 
@@ -7,10 +9,49 @@ namespace eegame{
 
 	public class TableManager : Singleton<TableManager> 
 	{
+		private const string Data_End = ".dat";
 
 		public TableManager()
 		{
 
+		}
+
+		public List<T> GetTableWithList<T>()
+		{
+			string file_path = DataEdit.FormatFileName(typeof(T).Name,FileNameType.RESOURCE_TABLE)+Data_End;
+			if (File.Exists (file_path))
+			{
+				using (var reader = new BinaryReader(File.OpenRead(file_path)))
+				{
+					var table = reader.ReadMarshalToList<T>();
+					return table;
+				}
+				
+			} 
+			else {
+				Debug.LogError("Data Not Exsit : "+file_path);
+			}
+
+			return null;
+		}
+
+		public Dictionary<uint,T> GetTableWithDic<T>() where T : IDicTable
+		{
+			string file_path = DataEdit.FormatFileName(typeof(T).Name,FileNameType.RESOURCE_TABLE)+Data_End;
+			if (File.Exists (file_path))
+			{
+				using (var reader = new BinaryReader(File.OpenRead(file_path)))
+				{
+					var table = reader.ReadMarshalToDic<T>();
+					return table;
+				}
+				
+			} 
+			else {
+				Debug.LogError("Data Not Exsit : "+file_path);
+			}
+
+			return null;
 		}
 
 	}
